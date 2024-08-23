e5b7fcb500030e66aff7cf725a395996da6d7d93
vojtechrichter <vojta3218@gmail.com>
2024-08-23 09:41:26 +0200

    read the source file from args

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "logger.h"
 
 static inline void separate_stdout_from_build_info()
 {
     printf("\n\t<---------------------------------------------------------->\n\n");
 }
 
-int main()
+int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        log_warning("Please provide source files you wish to compile.\n"); 
+        exit(1);
+    }
+
+    printf("source file: %s\n", argv[1]);
+
     separate_stdout_from_build_info();
 
     printf("\tHello from PL/0 Compiler!\n");
