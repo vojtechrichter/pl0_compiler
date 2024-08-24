946fd67cc710b844068d8fb41da5b4ffa0f522d9
vojtechrichter <vojta3218@gmail.com>
2024-08-24 14:35:05 +0200

    created tokenization function and implemented resolution for few tokens

--- src/tokenization/scanner.c
+++ src/tokenization/scanner.c
@@ -16,11 +16,11 @@ void read_src_file(const char *file_path, char *buffer)
 {
     size_t file_size = get_src_file_size(file_path);
 
-     int fd = open(file_path, O_RDWR, 0666);
-     size_t bytes_read = read(fd, buffer, file_size);
-     if (bytes_read == -1) {
-         handle_err("Failed to read the source file %s\n", file_path);
-     }
+    int fd = open(file_path, O_RDWR, 0666);
+    size_t bytes_read = read(fd, buffer, file_size);
+    if (bytes_read == -1) {
+        handle_err("Failed to read the source file %s\n", file_path);
+    }
 }
 
 Scanner *scanner_init(const char *file_path, Scanner *scanner)
@@ -99,7 +99,124 @@ void scanner_reset(Scanner *scanner)
     scanner->current_char = CURRCHAR(scanner);
 }
 
-char **scanner_tokenize_source(Scanner *scanner)
+uint32_t resolve_token(uint8_t c, Scanner *scanner)
 {
-    return (char **)0;
+    switch (c) {
+        case 'v': {
+            uint8_t next_char = scanner_advance(scanner);
+            if (next_char == 'a') {
+                next_char = scanner_advance(scanner);
+                if (next_char == 'r') {
+                    return TOKEN_VAR;
+                }
+            }
+        }
+
+        case ',': {
+            return TOKEN_COMMA;
+        }
+
+        case ';': {
+            return TOKEN_SEMICOLON;
+        }
+
+        case 'b': {
+            uint8_t next_char = scanner_advance(scanner);
+            if (next_char == 'e') {
+                next_char = scanner_advance(scanner);
+                if (next_char == 'g') {
+                    next_char = scanner_advance(scanner);
+                    if (next_char == 'i') {
+                        next_char = scanner_advance(scanner);
+                        if (next_char == 'n') {
+                            return TOKEN_BEGIN;
+                        }
+                    }
+                }
+            }
+        }
+
+        case ':': {
+			uint8_t next_char = scanner_advance(scanner);
+			if (next_char == '=') {
+				return TOKEN_ASSIGNMENT;
+			}
+        }
+
+		case 'w': {
+			uint8_t next_char = scanner_advance(scanner);
+			if (next_char == 'h') {
+				next_char = scanner_advance(scanner);
+				if (next_char == 'i') {
+					next_char = scanner_advance(scanner);
+					if (next_char == 'l') {
+						next_char = scanner_advance(scanner);
+						if (next_char == 'e') {
+							return TOKEN_WHILE;
+						}
+					}
+				}
+			}
+		}
+
+		case 'd': {
+            uint8_t next_char = scanner_advance(scanner);
+            if (next_char == 'o') {
+                return TOKEN_DO;
+            }
+		}
+
+        case '<': {
+            return TOKEN_LESSTHAN;
+        }
+
+        case '>': {
+            return TOKEN_GREATERTHAN;
+        }
+
+        case '*': {
+            return TOKEN_STAR;
+        }
+
+        case '/': {
+            return TOKEN_FSLASH;
+        }
+
+        case '+': {
+            return TOKEN_PLUS;
+        }
+
+        case '-': {
+            return TOKEN_MINUS;
+        }
+
+        case '=': {
+            return TOKEN_EQUAL;
+        }
+
+        case 'e': {
+            uint8_t next_char = scanner_advance(scanner);
+            if (next_char == 'n') {
+                next_char = scanner_advance(scanner);
+                if (next_char == 'd') {
+                    return TOKEN_END;
+                }
+            }
+        }
+
+        case '.': {
+            return TOKEN_DOT;
+        }
+    }
+}
+
+uint32_t *scanner_tokenize_source(Scanner *scanner)
+{
+    uint32_t *tokens = (uint32_t *)malloc(scanner->src_size * sizeof(uint32_t));
+
+    while (scanner->idx != (src_size - 1)) {
+        tokens[scanner->idx] = resolve_token(scanner->current_char, scanner);
+    }
+
+    return tokens;
 }
